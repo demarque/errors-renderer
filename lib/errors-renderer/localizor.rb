b9a0da7193f4474da79b434d753a799e9e417518
Philippe Rathé <prathe@gmail.com>
2014-04-28 11:50:10 -0400

    Fix I18n.t to always look in the default namespace.

--- lib/errors-renderer/localizor.rb
+++ lib/errors-renderer/localizor.rb
@@ -1,11 +1,11 @@
 module ErrorsRenderer
   module Localizor
     def self.get_message_from_code(code)
-      I18n.t('errors.messages')[code.to_sym]
+      I18n.t('default.errors.messages')[code.to_sym]
     end
 
     def self.get_code_from_message(message)
-      I18n.t('errors.messages').invert[message]
+      I18n.t('default.errors.messages').invert[message]
     end
   end
 end
